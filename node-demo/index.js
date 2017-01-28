4b9c856ddac7163203ed0dc3eedf40fa5a6b4712
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-01-28 12:00:28 -0600

    added info on command-line arguments

--- node-demo/index.js
+++ node-demo/index.js
@@ -4,5 +4,5 @@ const chalk = require('chalk');
 // The first value is the path to the node executable.
 // The second value is the path to the file being executed.
 // The third value (at index 2) is the first command-line argument.
-const name = process.argv[2] || 'World';
+const name = process.argv[2] || process.env.name || 'World';
 console.log('Hello, ' + chalk.red.bold(name) + '!');
